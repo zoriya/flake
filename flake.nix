a5a8d975b429d8d303b1265de2947966bcd89c29
Zoe Roux <zoe.roux@zoriya.dev>
2023-02-12 00:26:52 +0900

    Fix hypr (stopped using home manager)

--- flake.nix
+++ flake.nix
@@ -13,49 +13,49 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, ... }@input:
-    let
-      user = "zoriya";
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    hyprland,
+    ...
+  }: let
+    user = "zoriya";
 
-      lib = nixpkgs.lib;
+    lib = nixpkgs.lib;
 
-      mkSystem = pkgs: system: hostname: modules:
-        let
-  	  pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-	in
-        lib.nixosSystem {
-          inherit system;
-	  modules = [
-	    { networking.hostName = hostname; }
-            ./nixos/configuration.nix
+    mkSystem = pkgs: system: hostname: modules:
+      lib.nixosSystem {
+        inherit system;
+        modules = [
+          {networking.hostName = hostname;}
+          ./nixos/configuration.nix
 
-	    hyprland.nixosModules.default
+          hyprland.nixosModules.default
+{ programs.hyprland.enable = true; }
 
-	    home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = { inherit user; };
-                users.${user} = { config, ... }: { 
-		  imports = [
-		    ./modules/default.nix
-		    hyprland.homeManagerModules.default
-		  ];
-		  config.modules = modules;
-                };
-              };
-            }
-	  ];
-	};
-    in {
-      nixosConfigurations = {
-        fuhen = mkSystem nixpkgs "x86_64-linux" "fuhen" {
-          hyprland.enable = true;
-	};
+       #   home-manager.nixosModules.home-manager
+       #   {
+       #     home-manager = {
+       #       useGlobalPkgs = true;
+       #       useUserPackages = true;
+       #       extraSpecialArgs = {inherit user;};
+       #       users.${user} = {config, ...}: {
+       #         imports = [
+       #           ./modules/default.nix
+       #           hyprland.homeManagerModules.default
+       #         ];
+       #         config.modules = modules;
+       #       };
+       #     };
+       #   }
+        ];
+      };
+  in {
+    nixosConfigurations = {
+      fuhen = mkSystem nixpkgs "x86_64-linux" "fuhen" {
+        hyprland.enable = true;
       };
     };
+  };
 }
--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,15 +1,23 @@
-{ lib, config, ... }:
-let
-	cfg = config.modules.hyprland;
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.modules.hyprland;
 in {
-	options.modules.hyprland = { enable = lib.mkEnableOption "hyprland"; };
-	config = lib.mkIf cfg.enable
-		{
-			wayland.windowManager.hyprland = {
-				enable = true;
-				extraConfig = builtins.readFile ./hyprland.conf;
-			};
-			#home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
-		}
-	;
+  options.modules.hyprland = {enable = lib.mkEnableOption "hyprland";};
+  config =
+    lib.mkIf cfg.enable
+    {
+      wayland.windowManager.hyprland = {
+        enable = true;
+        systemdIntegration = true;
+        xwayland = {
+          enable = true;
+          hidpi = true;
+        };
+        nvidiaPatches = false;
+        extraConfig = builtins.readFile ./hyprland.conf;
+      };
+    };
 }
