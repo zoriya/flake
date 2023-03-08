ea71480f76f6a65bc90ef6cd315901276b0704ed
Zoe Roux <zoe.roux@zoriya.dev>
2023-03-08 22:00:37 +0900

    Update nixify

--- modules/home/zsh/custom.zsh
+++ modules/home/zsh/custom.zsh
@@ -7,7 +7,24 @@ push()
 	git add -A && git commit -m "$*" && git push
 }
 
-nixify()
+nixify() {
+	if [ ! -e ./.envrc ]; then
+		echo "use nix" > .envrc
+	fi
+	if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+		cat > default.nix <<'EOF'
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+	packages = with pkgs; [
+
+	];
+}
+EOF
+	fi
+	direnv allow
+}
+
+flakify()
 {
 	if [ ! -e flake.nix ]; then
 		nix flake new -t github:nix-community/nix-direnv .
