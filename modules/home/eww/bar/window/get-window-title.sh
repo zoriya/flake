d98809f64c98737d7f71ca73f5b3f6415c1c273e
Zoe Roux <zoe.roux@zoriya.dev>
2023-04-24 14:18:05 +0900

    Fix window name timeout

--- modules/home/eww/bar/window/get-window-title.sh
+++ modules/home/eww/bar/window/get-window-title.sh
@@ -3,5 +3,5 @@
 MAXCHAR=40
 
 hyprctl activewindow -j | jq --raw-output .title | cut -c -$MAXCHAR
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | grep --line-buffered '^activewindow>>' | stdbuf -o0 awk -F '>>|,' '{print $3}' | stdbuf -oL cut -c -$MAXCHAR
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | grep --line-buffered '^activewindow>>' | stdbuf -o0 awk -F '>>|,' '{printf "%.40s\n", $3}'
 
