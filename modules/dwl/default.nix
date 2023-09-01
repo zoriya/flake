432773387141d094b492d9a681d8a14764b5a001
Zoe Roux <zoe.roux@zoriya.dev>
2023-09-01 15:39:30 +0200

    Add polkit

--- modules/dwl/default.nix
+++ modules/dwl/default.nix
@@ -30,6 +30,21 @@
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
+  security.polkit.enable = true;
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = ["graphical-session.target"];
+    wants = ["graphical-session.target"];
+    after = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
   programs.dconf.enable = true;
   services.dbus.enable = true;
   xdg.portal = {
