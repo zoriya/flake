d0ed5735d1bb22632f3588ca07768a9e49713b4c
Zoe Roux <zoe.roux@zoriya.dev>
2023-10-26 19:14:48 +0000

    Simplify smartrss

--- modules/server/smartrss.sh
+++ modules/server/smartrss.sh
@@ -3,10 +3,10 @@ set -e
 
 name=$(guessit "$1" -P "title")
 
-curl -s 'http://localhost:3000/api/torrents' -b $(cat /var/lib/flood/flood.cookiefile) |
-	jq '.torrents.[].directory' -r |
-	sed s@/mnt/kyoo/shows/@@ |
-	grep -qix "$name"
+# curl -s 'http://localhost:3000/api/torrents' -b $(cat /var/lib/flood/flood.cookiefile) |
+# 	jq '.torrents.[].directory' -r |
+#	sed s@/mnt/kyoo/shows/@@ |
+ls /mnt/kyoo/shows/ | grep -qix "$name"
 
 echo "Downloading $1"
 exit 80
