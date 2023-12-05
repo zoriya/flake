363cd1e4535bd9b16447a83d52455c76b72e04d9
Zoe Roux <zoe.roux@zoriya.dev>
2023-12-06 00:52:58 +0100

    Setup better oil bindings

--- modules/misc/nvim/lua/plugins/tree.lua
+++ modules/misc/nvim/lua/plugins/tree.lua
@@ -2,15 +2,24 @@ return {
 	{
 		"stevearc/oil.nvim",
 		lazy = false,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+		},
 		opts = {
 			skip_confirm_for_simple_edits = true,
 			view_options = {
 				show_hidden = true,
 			},
-		},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		keys = {
-			{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+			keymaps = {
+				["<C-v>"] = "actions.select_vsplit",
+				["<C-s>"] = "actions.select_split",
+				["<BS>"] = "actions.parent",
+
+				-- Disable default mappings that conflict with harpoon.
+				["<C-h>"] = false,
+				["<C-t>"] = false,
+			},
 		},
 	},
 }
