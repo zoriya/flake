c94a4136fb70852fe54f835cc06d42d9aa6ecbbf
Zoe Roux <zoe.roux@zoriya.dev>
2024-06-10 12:29:22 +0200

    Enable spellchecking

--- modules/misc/nvim/lua/settings.lua
+++ modules/misc/nvim/lua/settings.lua
@@ -20,6 +20,7 @@ local options = {
 	sidescrolloff = 8,
 	wrap = false,
 	linebreak = true, -- When using set wrap, do not break in the middle of a word.
+	spell = true,
 
 	termguicolors = true,
 	swapfile = false,
