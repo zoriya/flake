67d07f7450a1cfb3d8cf53f8eb5f38ce9d2f87c2
Zoe Roux <zoe.roux@zoriya.dev>
2024-07-10 19:16:02 +0700

    Fix nvim not following system theme

--- modules/cli/nvim/default.nix
+++ modules/cli/nvim/default.nix
@@ -9,6 +9,8 @@
     extraPackages = with pkgs; [
       gcc
       tree-sitter
+      # Give access to gdbus for color-scheme detection (vim-lumen).
+      glib
     ];
   };
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/projects/flake/modules/cli/nvim";
