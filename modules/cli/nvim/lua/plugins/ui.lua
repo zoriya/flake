2bfb38b5fbf3c51027524c75685272d55e125bfd
Zoe Roux <zoe.roux@zoriya.dev>
2024-07-28 23:00:04 +0200

    Enable noice markdown

--- modules/cli/nvim/lua/plugins/ui.lua
+++ modules/cli/nvim/lua/plugins/ui.lua
@@ -62,11 +62,6 @@ return {
 				signature = { enabled = false },
 				hover = { enabled = false },
 				message = { enabled = false },
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-					["vim.lsp.util.stylize_markdown"] = false,
-					["cmp.entry.get_documentation"] = false,
-				},
 			},
 			views = {
 				mini = {
