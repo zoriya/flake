f63b97d7b810f5119f5c73a6ad1bf5b5bd2aea12
Zoe Roux <zoe.roux@zoriya.dev>
2024-12-21 17:11:29 +0100

    Move back to mainline home-manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:zoriya/home-manager";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
