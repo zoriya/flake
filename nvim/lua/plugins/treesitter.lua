03cd8acee2ca00b5967b4570e82221566f1665b3
Zoe Roux <zoe.roux@zoriya.dev>
2024-12-27 18:09:52 +0100

    Configure treesitter textobjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -11,12 +11,35 @@ return {
 			},
 			indent = {
 				enable = true
-			}
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["ia"] = "@parameter.inner",
+						["aa"] = "@parameter.outer",
+					},
+				},
+			},
 		},
 		after = function(plug)
 			require("nvim-treesitter.configs").setup(plug.opts)
 			vim.wo.foldmethod = 'expr'
 			vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 		end
+	},
+
+	{
+		"ts-comments.nvim",
+		lazy = false,
+		load = function() end,
+		after = function()
+			require("ts-comments").setup({})
+		end,
 	},
 }
