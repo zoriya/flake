182d929b7df5559de052312f90dcde929287a186
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-08 18:04:25 +0100

    Add git commit binding

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,14 +7,21 @@ return {
 			{ "<leader>f",  "<cmd>Telescope find_files<cr>",        desc = "Find Files" },
 			{ "<leader>F",  "<cmd>Telescope ripgrep theme=ivy<cr>", desc = "Grep" },
 			{ "<leader>gl", "<cmd>Telescope git_commits<CR>",       desc = "Git log" },
+			{ "<leader>gc", "<cmd>Git commit<CR>",                  desc = "Git commit" },
+			{ "<leader>gC", "<cmd>Git commit --amend<CR>",         desc = "Git commit amend" },
 			{ "<leader>gh", "<cmd>Telescope git_bcommits<CR>",      desc = "Git history" },
 			{ "<leader>gB", "<cmd>Telescope git_branches<CR>",      desc = "Git branches" },
 			{ "<leader>gs", "<cmd>Telescope git_status<CR>",        desc = "Git status" },
-			{ "<leader>gc", "<cmd>Telescope git_show<CR>",          desc = "Show last commit" },
 			{ "<leader>zh", "<cmd>Telescope help_tags<CR>",         desc = "Read help" },
 		},
 		after = function()
 			local actions = require("telescope.actions")
+			local action_state = require("telescope.actions.state")
+
+			local function git_show()
+				local entry = action_state.get_selected_entry()
+				vim.cmd("Telescope git_show ref=" .. entry.value)
+			end
 
 			local telescope = require("telescope")
 			telescope.setup({
