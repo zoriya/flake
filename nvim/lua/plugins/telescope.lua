7ca3d65b02c3670cae95c958da29100b31415f3a
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-08 17:33:47 +0100

    Handle path filters on telescope grep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,14 +4,14 @@ return {
 		load = vim.cmd.packadd,
 		cmd = "Telescope",
 		keys = {
-			{ "<leader>f",  "<cmd>Telescope find_files<cr>",          desc = "Find Files" },
-			{ "<leader>F",  "<cmd>Telescope live_grep theme=ivy<cr>", desc = "Grep" },
-			{ "<leader>gl", "<cmd>Telescope git_commits<CR>",         desc = "Git log" },
-			{ "<leader>gh", "<cmd>Telescope git_bcommits<CR>",        desc = "Git history" },
-			{ "<leader>gB", "<cmd>Telescope git_branches<CR>",        desc = "Git branches" },
-			{ "<leader>gs", "<cmd>Telescope git_status<CR>",          desc = "Git status" },
-			{ "<leader>gc", "<cmd>Telescope git_show<CR>",            desc = "Show last commit" },
-			{ "<leader>zh", "<cmd>Telescope help_tags<CR>",           desc = "Read help" },
+			{ "<leader>f",  "<cmd>Telescope find_files<cr>",        desc = "Find Files" },
+			{ "<leader>F",  "<cmd>Telescope ripgrep theme=ivy<cr>", desc = "Grep" },
+			{ "<leader>gl", "<cmd>Telescope git_commits<CR>",       desc = "Git log" },
+			{ "<leader>gh", "<cmd>Telescope git_bcommits<CR>",      desc = "Git history" },
+			{ "<leader>gB", "<cmd>Telescope git_branches<CR>",      desc = "Git branches" },
+			{ "<leader>gs", "<cmd>Telescope git_status<CR>",        desc = "Git status" },
+			{ "<leader>gc", "<cmd>Telescope git_show<CR>",          desc = "Show last commit" },
+			{ "<leader>zh", "<cmd>Telescope help_tags<CR>",         desc = "Read help" },
 		},
 		after = function()
 			local actions = require("telescope.actions")
@@ -44,6 +44,7 @@ return {
 			})
 
 			telescope.load_extension("fzf")
+			telescope.load_extension("ripgrep")
 		end
 	},
 }
