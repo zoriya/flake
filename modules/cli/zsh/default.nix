37de1a65b329ef639a84738c8fca224d886ff364
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-10 23:00:20 +0100

    Use ghostty's hm module

--- modules/cli/zsh/default.nix
+++ modules/cli/zsh/default.nix
@@ -257,7 +257,7 @@
       ''
         # Create a new tmux session (with a random name) and attach.
         if [[ -z "$TMUX" ]]; then
-        	exec tmux -u new-session -s "#$(hexdump -n 4 -v -e '/1 "%02X"' /dev/urandom)"
+          exec tmux -u new-session -s "#$(hexdump -n 4 -v -e '/1 "%02X"' /dev/urandom)"
         elif [[ $SHLVL -eq 1 ]]; then
           session=$(tmux display-message -p "#S")
           # kill current sesion if we are quiting the only pane
@@ -280,9 +280,9 @@
         # - 'mh+24' matches files (or directories or whatever) that are older than 24 hours.autoload -Uz compinit
         autoload -Uz compinit
         if [[ -n $ZSH_CACHE_DIR/.zcompdump(#qN.mh+24) ]]; then
-        	compinit;
+          compinit;
         else
-        	compinit -C;
+          compinit -C;
         fi;
       '';
     initExtra = builtins.readFile ./init.zsh;
--- modules/gui/ghostty.nix
+++ modules/gui/ghostty.nix
@@ -1,12 +1,55 @@
 {
   ghostty,
   pkgs,
-  lib,
   ...
 }: {
-  xdg.configFile."ghostty/config".source = ./ghostty.config;
+  programs.ghostty = {
+    enable = true;
+    package = ghostty.packages.${pkgs.system}.default;
+    enableZshIntegration = true;
+    clearDefaultKeybinds = true;
+    settings = {
+      font-size = 12;
+      font-feature = "-calt";
+      freetype-load-flags = false;
+      font-style = "semibold";
 
-  home.packages = lib.optionals pkgs.stdenv.isLinux [
-    ghostty.packages.${pkgs.system}.default
-  ];
+      theme = "light:catppuccin-latte,dark:catppuccin-mocha";
+      # disabling cursor because it sets it to blink and i hate that
+      shell-integration-features = "no-cursor,sudo,title";
+      cursor-style-blink = false;
+
+      window-padding-x = 0;
+      window-padding-y = 0;
+      window-padding-balance = true;
+
+      window-decoration = false;
+      confirm-close-surface = false;
+      quit-after-last-window-closed = true;
+      resize-overlay = "never";
+      # avoid dreadfully long startup times
+      gtk-single-instance = true;
+      auto-update = "off";
+      unfocused-split-opacity = 1;
+
+      clipboard-read = "allow";
+      copy-on-select = false;
+
+      keybind = [
+        "ctrl+shift+c=copy_to_clipboard"
+        "ctrl+shift+v=paste_from_clipboard"
+        "cmd+c=copy_to_clipboard"
+        "cmd+v=paste_from_clipboard"
+
+        "ctrl+plus=increase_font_size:1"
+        "ctrl+equal=increase_font_size:1"
+        "ctrl+minus=decrease_font_size:1"
+        "ctrl+zero=reset_font_size"
+        "ctrl+backspace=reset_font_size"
+
+        "ctrl+shift+comma=reload_config"
+        "ctrl+shift+i=inspector:toggle"
+      ];
+    };
+  };
 }
