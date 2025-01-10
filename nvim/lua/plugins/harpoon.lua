6623a600b915836c15d220e72df85ead0fd8aa67
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-11 00:31:26 +0100

    Fix harpoon binding conflict with gitsigns

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -3,7 +3,7 @@ return {
 		"harpoon2",
 		keys = {
 			{ "<leader>a", '<cmd>lua require("harpoon"):list():add()<CR>',                                    desc = "Mark file" },
-			{ "<leader>h", '<cmd>lua require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())<CR>', desc = "Harpoon menu" },
+			{ "<leader>m", '<cmd>lua require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())<CR>', desc = "Harpoon menu" },
 			{ "<A-h>",     '<cmd>lua require("harpoon"):list():select(1)<CR>',                                desc = "Navigate to harpoon 1" },
 			{ "<A-t>",     '<cmd>lua require("harpoon"):list():select(2)<CR>',                                desc = "Navigate to harpoon 2" },
 			{ "<A-n>",     '<cmd>lua require("harpoon"):list():select(3)<CR>',                                desc = "Navigate to harpoon 3" },
