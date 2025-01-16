c3a9810ae8aee60272f25bcc16171157bfc5839c
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-16 18:05:30 +0100

    Fix harppon setup

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -2,25 +2,26 @@ return {
 	{
 		"harpoon2",
 		keys = {
-			{ "<leader>a", '<cmd>lua require("harpoon"):list():add()<CR>',                                    desc = "Mark file" },
+			{ "<leader>a", '<cmd>lua require("harpoon"):list():add()<CR>', desc = "Mark file" },
 			{ "<leader>m", '<cmd>lua require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())<CR>', desc = "Harpoon menu" },
-			{ "<A-h>",     '<cmd>lua require("harpoon"):list():select(1)<CR>',                                desc = "Navigate to harpoon 1" },
-			{ "<A-t>",     '<cmd>lua require("harpoon"):list():select(2)<CR>',                                desc = "Navigate to harpoon 2" },
-			{ "<A-n>",     '<cmd>lua require("harpoon"):list():select(3)<CR>',                                desc = "Navigate to harpoon 3" },
-			{ "<A-s>",     '<cmd>lua require("harpoon"):list():select(4)<CR>',                                desc = "Navigate to harpoon 4" },
+			{ "<A-h>", '<cmd>lua require("harpoon"):list():select(1)<CR>', desc = "Navigate to harpoon 1" },
+			{ "<A-t>", '<cmd>lua require("harpoon"):list():select(2)<CR>', desc = "Navigate to harpoon 2" },
+			{ "<A-n>", '<cmd>lua require("harpoon"):list():select(3)<CR>', desc = "Navigate to harpoon 3" },
+			{ "<A-s>", '<cmd>lua require("harpoon"):list():select(4)<CR>', desc = "Navigate to harpoon 4" },
 
 			-- <C-;> is not a valid sequence so use HJKL instead.
-			{ "<A-j>",     '<cmd>lua require("harpoon"):list():select(2)<CR>',                                desc = "Navigate to harpoon 2" },
-			{ "<A-k>",     '<cmd>lua require("harpoon"):list():select(3)<CR>',                                desc = "Navigate to harpoon 3" },
+			{ "<A-j>", '<cmd>lua require("harpoon"):list():select(2)<CR>', desc = "Navigate to harpoon 2" },
+			{ "<A-k>", '<cmd>lua require("harpoon"):list():select(3)<CR>', desc = "Navigate to harpoon 3" },
 			-- <C-L> is already taken but since I use harpoon less on querty no worry
 		},
 		opts = {
 			settings = {
 				save_on_toggle = true,
+				sync_on_ui_close = true,
 			},
 		},
 		after = function(plug)
-			require("harpoon"):setup(plug.otps)
+			require("harpoon"):setup(plug.opts)
 		end,
 	},
 }
