8242c7e0d192c4e76d804ae4e24a8ddd1b9ee926
Zoe Roux <zoe.roux@zoriya.dev>
2025-05-10 02:16:42 +0200

    Match all sql like strings as sql

--- nvim/after/queries/python/injections.scm
+++ nvim/after/queries/python/injections.scm
@@ -3,3 +3,8 @@
   function: (attribute attribute: (identifier) @id (#match? @id "execute|read_sql"))
   arguments: (argument_list
     (string (string_content) @injection.content (#set! injection.language "sql"))))
+
+(string 
+  (string_content) @injection.content
+    (#match? @injection.content "\\s*(SELECT|CREATE|DROP|INSERT|UPDATE|ALTER|DELETE|select|create|drop|insert|update|alter|delete)\\W+")
+    (#set! injection.language "sql"))
