4bf32f7854259f7fc546ce53a619bafd751f0db3
Zoe Roux <zoe.roux@zoriya.dev>
2025-06-14 16:59:38 +0200

    I hate firewall

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -6,4 +6,7 @@
   ];
   hardware.steam-hardware.enable = true;
   services.flatpak.enable = true;
+
+  # i lost way too much time understanding why my local server/app can't be reached
+  networking.firewall.enable = false;
 }
