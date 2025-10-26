c3f187706736b2bdc6c0894d1eeac55233db8792
Zoe Roux <zoe.roux@zoriya.dev>
2025-10-26 11:41:08 +0100

    Disable on type formatting of biome

--- nvim/after/lsp/biome.lua
+++ nvim/after/lsp/biome.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(args)
+		local client_id = args.data.client_id
+		local client = assert(vim.lsp.get_client_by_id(client_id))
+		if client.name == 'biome' then
+			vim.lsp.on_type_formatting.enable(false, { client_id = client_id })
+		end
+	end,
+})
+
 return {
 	-- Disable lunching from node_modules (no nix binary)
 	cmd = { "biome", "lsp-proxy" },
