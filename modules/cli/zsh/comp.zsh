3cfbd8f495ab4799b13df995753f05ed583e7355
Zoe Roux <zoe.roux@zoriya.dev>
2026-07-23 15:53:04 +0200

    Fix biome formating

--- modules/cli/zsh/comp.zsh
+++ modules/cli/zsh/comp.zsh
@@ -1,7 +1,7 @@
 # default ls colors
 LS_COLORS=${LS_COLORS:-'di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'}
 
-# case insensitive completion & enable completion from the middle of the word 
+# case insensitive completion & enable completion from the middle of the word
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|=* r:|=*'
 
 # allow completions in commands starting with sudo
--- nvim/after/lsp/biome.lua
+++ nvim/after/lsp/biome.lua
@@ -14,4 +14,16 @@ return {
 	cmd = { "biome", "lsp-proxy" },
 	-- for json files
 	workspace_required = false,
+	-- The default lspconfig root_dir only starts biome when a biome.json exists,
+	-- so it never attaches for config-less projects or standalone files. Always
+	-- resolve a root so biome attaches everywhere
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		local root_files = { "biome.json", "biome.jsonc" }
+		-- biome recommends searching from cwd for monorepos
+		local found = vim.fs.find(root_files, { path = vim.fn.getcwd(), upward = true })[1]
+			or vim.fs.find(root_files, { path = fname, upward = true })[1]
+		-- fall back to the file's own dir so a standalone json still gets a workspace
+		on_dir(found and vim.fs.dirname(found) or vim.fs.dirname(fname))
+	end,
 }
